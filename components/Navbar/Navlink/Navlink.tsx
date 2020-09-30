3c7955b60579fe4fdbb57468baa02c3cbdefa78c
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-09-30 17:58:09 +0700

    NavLink : nav items depend on user login

--- components/Navbar/Navlink/Navlink.tsx
+++ components/Navbar/Navlink/Navlink.tsx
@@ -1,28 +1,37 @@
 import React from "react";
+import { useAuth } from "../../Hooks/use-auth";
 import Link from "../../utils/Link";
 import styles from "./Navlink.module.css";
 
 const Navlink = () => {
+  const auth = useAuth();
+
   return (
     <ul className={styles.nav}>
       <Link href="/requests">
         <li>Request</li>
       </Link>
-      <Link href="/history">
-        <li>History</li>
-      </Link>
-      <Link href="/about">
+      {auth.user && (
+        <>
+          <Link href="/history">
+            <li>History</li>
+          </Link>
+          <li onClick={() => auth.signout()}>Logout</li>
+        </>
+      )}
+      {!auth.user && (
+        <>
+          <Link href="/signup">
+            <li>Signup</li>
+          </Link>
+          <Link href="/login">
+            <li>Login</li>
+          </Link>
+        </>
+      )}
+      {/* <Link href="/about">
         <li>About</li>
-      </Link>
-      <Link href="/signup">
-        <li>Signup</li>
-      </Link>
-      <Link href="/login">
-        <li>Login</li>
-      </Link>
-      <Link href="/logout">
-        <li>Logout</li>
-      </Link>
+      </Link> */}
     </ul>
   );
 };
