23c650ed11f7610d8b102a323a02d347f1ec8616
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-02 13:35:14 +0700

    Request Donor Success : styling and routing

--- components/RequestDonor/RequestDonorSuccess/RequestDonorSuccess.tsx
+++ components/RequestDonor/RequestDonorSuccess/RequestDonorSuccess.tsx
@@ -1,8 +1,24 @@
+import Link from "next/link";
 import React from "react";
 import styles from "./RequestDonorSuccess.module.css";
 
 const RequestDonorSuccess = () => {
-  return <div>From Request Donor Success Screen</div>;
+  return (
+    <div className={styles.container}>
+      <img
+        src="/image/success-icon.svg"
+        alt="success-icon"
+        className={styles.icon}
+      />
+      <h3 className={styles.title}>Request Posted</h3>
+      <p className={styles.text}>
+        You can see your request in the Request Post List
+      </p>
+      <Link href="/">
+        <button className={styles.button}>Okay</button>
+      </Link>
+    </div>
+  );
 };
 
 export default RequestDonorSuccess;
