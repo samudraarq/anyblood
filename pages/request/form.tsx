0461ab3f516d578ed0d3b98cdc02c254870de2ac
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-02 09:31:53 +0700

    request form page

---
+++ pages/request/form.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
+
+const form = () => {
+  return (
+    <>
+      <div className="container">
+        <Navbar />
+        From request form
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default form;
