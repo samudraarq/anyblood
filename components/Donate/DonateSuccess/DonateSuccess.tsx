e1c97617c74132289f98476c52f97a7b98e833d4
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-02 09:20:15 +0700

    success screen : add styling and content

--- components/Donate/DonateSuccess/DonateSuccess.tsx
+++ components/Donate/DonateSuccess/DonateSuccess.tsx
@@ -1,8 +1,24 @@
+import Link from "next/link";
 import React from "react";
 import styles from "./DonateSuccess.module.css";
 
 const DonateSuccess = () => {
-  return <div>From donate success screen</div>;
+  return (
+    <div className={styles.container}>
+      <img
+        src="/image/success-icon.svg"
+        alt="success-icon"
+        className={styles.icon}
+      />
+      <h3 className={styles.title}>Registration Success</h3>
+      <p className={styles.text}>
+        Please come to your nearest PMI on the selected date
+      </p>
+      <Link href="/">
+        <button className={styles.button}>Okay</button>
+      </Link>
+    </div>
+  );
 };
 
 export default DonateSuccess;
