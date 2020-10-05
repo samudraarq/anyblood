e45e399b3a294cb32ff34ca849b366e917e7231d
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-05 09:03:41 +0700

    RequestDonorFormik : styled components

--- components/Formik/RequestDonorFormik.tsx
+++ components/Formik/RequestDonorFormik.tsx
@@ -2,7 +2,79 @@ import React from "react";
 import { Field, Form, Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { ChevronRight, User } from "react-feather";
-import styles from "./Formik.module.css";
+import styled from "styled-components";
+
+const InputContainer = styled.div`
+  width: 100%;
+  position: relative;
+  margin-bottom: 2.4rem;
+`;
+
+const Icon = styled.span`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 1.5rem;
+`;
+
+const StyledField = styled(Field)`
+  width: 100%;
+  height: 5.6rem;
+  padding-left: 5.2rem;
+  border: 2px solid #18191f;
+  border-radius: 16px;
+  font-family: inherit;
+  font-size: 2.1rem;
+  color: #18191f;
+  outline: none;
+
+  /* Remove number input arrow */
+  /* Chrome, Safari, Edge, Opera */
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  &[type="number"] {
+    -moz-appearance: textfield;
+  }
+`;
+
+const Alert = styled.div`
+  color: #f95a2c;
+  padding-left: 2rem;
+  margin-top: -2rem;
+  margin-bottom: 1.5rem;
+  font-size: 1.3rem;
+`;
+
+const SendForm = styled.button`
+  width: 33rem;
+  height: 6rem;
+  background: #18191f;
+  border: none;
+  border-radius: 16px;
+  font-family: inherit;
+  font-weight: 800;
+  font-size: 2.1rem;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: 0;
+  margin-top: 4rem;
+
+  & > span {
+    margin-right: 1rem;
+    transform: translateY(-1px);
+  }
+`;
 
 const RequestDonorFormik = ({ handleSubmit }) => {
   return (
@@ -26,80 +98,57 @@ const RequestDonorFormik = ({ handleSubmit }) => {
       }}
     >
       <Form>
-        <div className={styles.inputContainer}>
-          <span className={styles.icon}>
+        <InputContainer>
+          <Icon>
             <User />
-          </span>
-          <Field
-            name="fullname"
-            type="text"
-            placeholder="Full Name"
-            className={styles.input}
-          />
-        </div>
+          </Icon>
+          <StyledField name="fullname" type="text" placeholder="Full Name" />
+        </InputContainer>
         <ErrorMessage name="fullname">
-          {(msg) => <div className={styles.alert}>{msg}</div>}
+          {(msg) => <Alert>{msg}</Alert>}
         </ErrorMessage>
-        <div className={styles.inputContainer}>
-          <span className={styles.icon}>
+        <InputContainer>
+          <Icon>
             <User />
-          </span>
-          <Field
-            name="age"
-            type="number"
-            placeholder="Age"
-            className={styles.input}
-          />
-        </div>
-        <ErrorMessage name="age">
-          {(msg) => <div className={styles.alert}>{msg}</div>}
-        </ErrorMessage>
-        <div className={styles.inputContainer}>
-          <span className={styles.icon}>
+          </Icon>
+          <StyledField name="age" type="number" placeholder="Age" />
+        </InputContainer>
+        <ErrorMessage name="age">{(msg) => <Alert>{msg}</Alert>}</ErrorMessage>
+        <InputContainer>
+          <Icon>
             <User />
-          </span>
-          <Field
-            name="bloodtype"
-            type="text"
-            placeholder="Bloodtype"
-            className={styles.input}
-          />
-        </div>
+          </Icon>
+          <StyledField name="bloodtype" type="text" placeholder="Bloodtype" />
+        </InputContainer>
         <ErrorMessage name="bloodtype">
-          {(msg) => <div className={styles.alert}>{msg}</div>}
+          {(msg) => <Alert>{msg}</Alert>}
         </ErrorMessage>
-        <div className={styles.inputContainer}>
-          <span className={styles.icon}>
+        <InputContainer>
+          <Icon>
             <User />
-          </span>
-          <Field
+          </Icon>
+          <StyledField
             name="bloodbagsamount"
             type="number"
             placeholder="Blood Bags Amount"
-            className={styles.input}
           />
-        </div>
+        </InputContainer>
         <ErrorMessage name="bloodbagsamount">
-          {(msg) => <div className={styles.alert}>{msg}</div>}
+          {(msg) => <Alert>{msg}</Alert>}
         </ErrorMessage>
-        <div className={styles.inputContainer}>
-          <span className={styles.icon}>
+        <InputContainer>
+          <Icon>
             <User />
-          </span>
-          <Field
-            name="place"
-            type="text"
-            placeholder="Place"
-            className={styles.input}
-          />
-        </div>
+          </Icon>
+          <StyledField name="place" type="text" placeholder="Place" />
+        </InputContainer>
         <ErrorMessage name="place">
-          {(msg) => <div className={styles.alert}>{msg}</div>}
+          {(msg) => <Alert>{msg}</Alert>}
         </ErrorMessage>
-        <button type="submit" className={styles.sendform}>
+        <SendForm type="submit">
           <span>Send Form</span>
           <ChevronRight />
-        </button>
+        </SendForm>
       </Form>
     </Formik>
   );
