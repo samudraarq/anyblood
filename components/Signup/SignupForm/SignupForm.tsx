d99d938f48df3435197d3f7c6e9164ecedac1b27
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-05 10:51:12 +0700

    Sign up form : styled components

--- components/Signup/SignupForm/SignupForm.tsx
+++ components/Signup/SignupForm/SignupForm.tsx
@@ -1,10 +1,40 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import styles from "./SignupForm.module.css";
 import { useAuth } from "../../Hooks/use-auth";
 import { useRouter } from "next/router";
 import SignupFormik from "../../Formik/SignupFormik";
 import { db } from "../../../config/fbConfig";
+import styled from "styled-components";
+
+const Form = styled.div`
+  max-width: 45rem;
+
+  & > h2 {
+    font-size: 3.6rem;
+    font-weight: 800;
+    margin-bottom: 1.6rem;
+  }
+
+  & > p {
+    font-size: 1.7rem;
+    font-weight: 500;
+    margin-bottom: 4.8rem;
+  }
+`;
+
+const Error = styled.p`
+  color: #f95a2c;
+  margin-top: 1rem;
+`;
+
+const Account = styled.p`
+  margin-top: 3.2rem;
+
+  & > span {
+    color: #f95a2c;
+    cursor: pointer;
+  }
+`;
 
 const SignupForm = () => {
   const [signupError, setSignupError] = useState(false);
@@ -41,18 +71,18 @@ const SignupForm = () => {
   };
 
   return (
-    <div className={styles.form}>
+    <Form>
       <h2>Sign Up</h2>
       <p>Start helping other people by creating an account first.</p>
       <SignupFormik handleSubmit={handleSubmit} />
-      {signupError && <p className={styles.error}>{errorMsg}</p>}
-      <p className={styles.account}>
+      {signupError && <Error>{errorMsg}</Error>}
+      <Account>
         Already have account?
         <Link href="/login">
           <span> Go here</span>
         </Link>
-      </p>
-    </div>
+      </Account>
+    </Form>
   );
 };
 
