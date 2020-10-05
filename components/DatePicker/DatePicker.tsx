7ae31c3a908e40d77ae3109d810b07a44dd16876
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-05 08:37:07 +0700

    DatePicker styled components

--- components/DatePicker/DatePicker.tsx
+++ components/DatePicker/DatePicker.tsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { useField, useFormikContext } from "formik";
+import styled from "styled-components";
 import DatePicker from "react-datepicker";
 import styles from "./DatePicker.module.css";
 import addDays from "date-fns/addDays";
+
+const StyledDatePicker = styled(DatePicker)`
+  width: 100%;
+  height: 5.6rem;
+  padding-left: 5.2rem;
+  border: 2px solid #18191f;
+  border-radius: 16px;
+  font-family: inherit;
+  font-size: 2.1rem;
+  color: #18191f;
+  outline: none;
+  background-color: transparent;
+`;
 
 const DatePickerField = (props) => {
   const { setFieldValue } = useFormikContext();
   const [field] = useField(props);
 
   return (
-    <DatePicker
+    <StyledDatePicker
       {...field}
       {...props}
       selected={(field.value && new Date(field.value)) || null}
       onChange={(val) => {
         setFieldValue(field.name, val);
       }}
-      className={styles.date}
-      calendarClassName={styles.calendar}
       dateFormat="dd/MM/yyyy"
       placeholderText="Select a date"
       minDate={new Date()}
